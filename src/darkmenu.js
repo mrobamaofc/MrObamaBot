833db9a3962c215fcbbb0c469063ee2ecfe6836a
mrobamaofc <76693539+mrobamaofc@users.noreply.github.com>
2021-03-01 19:25:16 -0300

    Add files via upload

---
+++ src/darkmenu.js
@@ -0,0 +1,41 @@
+const darkmenu = (prefix) => {
+	return `
+
+            COMANDOS:
+
+
+  *Comandos do Obama:*
+
+➸ *${prefix}loli*
+➸ *${prefix}hentai*
+➸ *${prefix}dono*
+➸ *${prefix}porno*
+➸ *${prefix}boanoite*
+➸ *${prefix}bomdia*
+➸ *${prefix}boatarde*
+➸ *${prefix}mia*
+➸ *${prefix}mia1*
+➸ *${prefix}mia2*
+➸ *${prefix}belle*
+➸ *${prefix}belle1*
+➸ *${prefix}belle2*
+➸ *${prefix}belle3*
+➸ *${prefix}ayeko*
+
+╔════════════════════
+ 
+  DUVIDAS? 👇
+  chama zap
+  wa.me/553899815548
+╚════════════════════`
+}
+
+exports.darkmenu = darkmenu
+
+
+
+
+
+
+
+
